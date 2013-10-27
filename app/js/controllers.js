de6efa95974ffec962f0de9e5cab391414d898ed
Davide Cavaliere <cavaliere.davide@gmail.com>
2013-10-27 12:00:25 +0000

    First Cut

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,96 @@
 'use strict';
-
 /* Controllers */
-
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+    controller('MyCtrl1', ['$scope', function ($scope) {
 
-  }]);
+        function init() {
+
+            /*        var palette = [
+             'rgb(0,0,0)',
+             'rgb(255,0,0)',
+             'rgb(0,255,0)',
+             'rgb(0,0,255)',
+             'rgb(255,255,255)'
+             ];
+
+             var R = 1000;
+
+             var color = palette[1];
+
+             var canvas = document.getElementById("a");
+
+             var context = canvas.getContext("2d");
+
+             var math = mathjs();
+
+             context.strokeRect(1, 1, 500, 300);
+             context.strokeRect(125,40, 500, 300);
+             console.log("Start calculation");
+             for ( var x=0; x<canvas.width; x++ ){
+             for( var y=0; y<canvas.height; y++ ) {
+             var xtemp = 0.0;
+
+             var zx = 0.0;
+             var zy = 0.0;
+             var i = 0;
+
+             while ((zx*zx) + (zy*zy) < R && i<palette.length) {
+             var xtemp = zx*zx + zy*zy + y/2.5;
+             zy = 2*zx*zy + y/2.5 ;
+             zx = xtemp;
+
+             i++;
+             }
+             color = palette[i-1];
+
+             context.fillStyle = color;
+
+             context.fillRect(x,y,1,1);
+             }
+             }
+             console.log("calculation done");*/
+
+
+        }
+
+        function drawMandel() {
+            NProgress.start();
+            initMandel("canvas", 960, 440);
+
+
+            computeMandel();
+            NProgress.done();
+        }
+
+        init();
+        drawMandel();
+
+
+
+
+
+
+
+    }])
+
+
+    .controller('MyCtrl2', ['$scope', function ($scope) {
+        $scope.reset = function() {
+
+            console.log("resetting");
+
+            var worker = new Worker('js/reDraw.js');
+
+            worker.postMessage("start");
+
+        }
+
+        function reDraw() {
+           // NProgress.start();
+            initMandel('canvas', 960, 440);
+            computeMandel();
+
+            //NProgress.done();
+        }
+    }]);
---
+++ app/js/reDraw.js
@@ -0,0 +1,8 @@
+/**
+ * Created by damny on 27/10/13.
+ */
+//NProgress.start();
+initMandel('canvas', 960, 440);
+computeMandel();
+
+//NProgress.done();
